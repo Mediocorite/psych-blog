7cbb8e1af49aae39facd9f7ba8d0a5c0a983382c
Anzar Ahmad Malik <12586091+Mediocorite@users.noreply.github.com>
2024-01-13 03:29:37 +0000

    Feature: Dark mode theme toggle

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -1,7 +1,34 @@
-import React from 'react'
+"use client";
+import React, { useState, useEffect } from "react";
+import { Switch } from "@headlessui/react";
+import Image from "next/image";
 
-export default function ThemeToggle() {
+export default function ThemeSwitcher() {
+  const [darkTheme, setDarkTheme] = useState(false);
+
+  useEffect(() => {
+    document.body.classList.toggle("dark", darkTheme);
+  }, [darkTheme]);
+
   return (
-    <div>ThemeToggle</div>
-  )
+    <Switch
+      checked={darkTheme}
+      onChange={setDarkTheme}
+      className={`${darkTheme ? "bg-gray-700" : "bg-gray-200"}
+        relative inline-flex h-6 w-11 items-center rounded-full
+        transition-colors focus:outline-none`}
+    >
+      <span
+        className={`${darkTheme ? "translate-x-6" : "translate-x-1"}
+          inline-block h-4 w-4 transform rounded-full
+          bg-white p-0.5 transition-transform`}
+      >
+        {darkTheme ? (
+          <Image src="/moon.png" alt="moon" width={15} height={15} />
+        ) : (
+          <Image src="/sun.png" alt="sun" width={15} height={15} />
+        )}
+      </span>
+    </Switch>
+  );
 }
