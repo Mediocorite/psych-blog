ac225efaf3e2b963e94216178ba1a316b7d78d92
Anzar Ahmad Malik <12586091+Mediocorite@users.noreply.github.com>
2024-01-17 14:34:54 +0000

    Bugfix: Responsive menu

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,32 +3,67 @@ import Image from "next/image";
 import Link from "next/link";
 import ThemeToggle from "./ThemeToggle";
 import AuthLinks from "./AuthLinks";
-import Hamburger from "./Hamburger";
+import { Drawer, DrawerContent, DrawerTrigger } from "./ui/drawer";
+
+const links = [
+  { id: 1, name: "About", url: "/about" },
+  { id: 2, name: "Contact", url: "/contact" },
+  { id: 3, name: "Resume", url: "/resume" },
+];
 
 export default function Navbar() {
   return (
     <div className="flex h-28 items-center justify-between">
-      <div className="flex flex-1 text-center text-4xl font-bold md:text-2xl lg:text-left xl:text-3xl">
-        Jung and the Restless
+      <div className="flex flex-1 text-center text-lg font-bold md:text-2xl lg:text-left xl:text-3xl">
+        <Link href={"/"}>Jung and the Restless</Link>
       </div>
-      <div className="flex flex-1 items-center gap-5 text-lg sm:justify-end xl:text-lg">
+      <div className="hidden flex-1 items-center gap-5 text-lg sm:justify-end md:flex xl:text-lg">
         <ThemeToggle initialTheme="light" />
-        <Link href="/" className="hidden sm:flex">
-          Homepage
-        </Link>
-        <Link href="/" className="hidden sm:flex">
-          Contact
-        </Link>
-        <Link href="/" className="hidden sm:flex">
-          About
-        </Link>
+        {links.map((link) => (
+          <Link key={link.id} href={link.url}>
+            {link.name}
+          </Link>
+        ))}
         <AuthLinks />
         <div className=" hidden flex-1 gap-3 lg:flex">
           <Image src="/facebook.png" alt="facebook" width={24} height={24} />
           <Image src="/instagram.png" alt="instagram" width={24} height={24} />
         </div>
-        <Hamburger initialTheme="light" />
       </div>
+      <Drawer>
+        <DrawerTrigger className="flex md:hidden">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="icon icon-tabler icon-tabler-menu-2"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            stroke-width="2"
+            stroke="currentColor"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M4 6l16 0" />
+            <path d="M4 12l16 0" />
+            <path d="M4 18l16 0" />
+          </svg>
+        </DrawerTrigger>
+        <DrawerContent>
+          <div className="flex flex-col items-center justify-center">
+            {links.map((link) => (
+              <Link
+                key={link.id}
+                href={link.url}
+                className="m-4 p-4 text-center text-2xl"
+              >
+                {link.name}
+              </Link>
+            ))}
+          </div>
+        </DrawerContent>
+      </Drawer>
     </div>
   );
 }
