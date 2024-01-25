7b1fcd1bc75cf8b89732e20d8871c0304e0f74e6
Anzar Ahmad Malik <12586091+Mediocorite@users.noreply.github.com>
2024-01-25 19:00:59 +0000

    ProdBugfix: Removing export from options

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,45 @@
 import NextAuth from "next-auth";
-import { Options } from "@/utilities/authOptions";
 
-export const handler = NextAuth(Options);
+import CredentialsProvider from "next-auth/providers/credentials";
+import { NextAuthOptions } from "next-auth";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/middleware/firebase";
+const Options: NextAuthOptions = {
+  // Configure one or more authentication providers
+  pages: {
+    signIn: "/login",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "email", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials): Promise<any> {
+        console.log(`This is the creds: ` + JSON.stringify(credentials));
+
+        return await signInWithEmailAndPassword(
+          auth,
+          (credentials as any).email || "",
+          (credentials as any).password || "",
+        )
+          .then((userCredential) => {
+            if (userCredential.user) {
+              return userCredential.user;
+            }
+            return null;
+          })
+          .catch((error) => console.log(error))
+          .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(error);
+          });
+      },
+    }),
+  ],
+};
+const handler = NextAuth(Options);
 export { handler as GET, handler as POST };
