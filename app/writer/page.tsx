a154869733a95afdea9fcd77cb0f07fa48f6c0e5
Anzar Ahmad Malik <12586091+Mediocorite@users.noreply.github.com>
2024-01-25 15:42:33 +0000

    TODO: Planning the writer work

--- app/writer/page.tsx
+++ app/writer/page.tsx
@@ -3,17 +3,22 @@ import React from "react";
 import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 export default function Writer() {
+  // TODO: Make sure the page is inaccessible if not logged in.
+  // TODO: Install react-markdown.
+  // TODO: Hookup text area to be converted into markdown.
   const session = useSession({
     required: true,
     onUnauthenticated() {
       redirect("/login");
     },
   });
-  return (
-    <div>
-      <div className="">Writer</div>
-    </div>
-  );
+  if (session)
+    return (
+      <div>
+        <div className="">Writer</div>
+      </div>
+    );
+  return null;
 }
 
 Writer.requireAuth = true;
