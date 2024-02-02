e59602522434f84b26c86cb944c4236787b6ddef
Anzar Ahmad Malik <12586091+Mediocorite@users.noreply.github.com>
2024-02-02 23:55:38 +0000

    Configuring the db

---
+++ app/writer/components/multipleBak.ts
@@ -0,0 +1,51 @@
+{
+  /* <Listbox value={categoryList} onChange={setCategoryList}>
+            <div className="relative text-4xl">
+              <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white px-6 py-3 text-left text-lg shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 md:text-xl">
+                <span className="block truncate">{categoryList.name}</span>
+                <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                  👇
+                </span>
+              </Listbox.Button>
+              <Transition
+                as={Fragment}
+                leave="transition ease-in duration-100"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+                  {categories.map((category, categoryIdx) => (
+                    <Listbox.Option
+                      key={categoryIdx}
+                      className={({ active }) =>
+                        `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                          active
+                            ? "bg-amber-100 text-amber-900"
+                            : "text-gray-900"
+                        }`
+                      }
+                      value={category}
+                    >
+                      {({ selected }) => (
+                        <>
+                          <span
+                            className={`block truncate ${
+                              selected ? "font-medium" : "font-normal"
+                            }`}
+                          >
+                            {category.name}
+                          </span>
+                          {selected ? (
+                            <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                              ✅
+                            </span>
+                          ) : null}
+                        </>
+                      )}
+                    </Listbox.Option>
+                  ))}
+                </Listbox.Options>
+              </Transition>
+            </div>
+          </Listbox> */
+}
--- app/writer/page.tsx
+++ app/writer/page.tsx
@@ -1,43 +1,41 @@
 "use client";
-import React, { Fragment, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import Markdown from "react-markdown";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCirclePlus } from "@fortawesome/free-solid-svg-icons";
-import Image from "next/image";
-import { Listbox, Transition } from "@headlessui/react";
-import CategoryList from "../components/CategoryList";
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "@/middleware/firebase";
 import { CategorySelect } from "./components/CategorySelect";
-
-const categories = [
-  { id: 1, name: "Durward Reynolds", unavailable: false },
-  { id: 2, name: "Kenton Towne", unavailable: false },
-  { id: 3, name: "Therese Wunsch", unavailable: false },
-  { id: 4, name: "Benedict Kessler", unavailable: true },
-  { id: 5, name: "Katelyn Rohan", unavailable: false },
-];
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 export default function Writer() {
-  // TODO: Hookup text area to be converted into markdown.
-  // const { data: session } = useSession();
-  // const router = useRouter();
+  const { data: session } = useSession();
+  const router = useRouter();
 
-  // useEffect(() => {
-  //   if (!session) {
-  //     router.push("/");
-  //   }
-  // }, [session]);
-  // Have a simple markdown editor.
-  // Two components -> Editor window / Output windows
+  useEffect(() => {
+    if (!session) {
+      router.push("/");
+    }
+  }, [session]);
 
-  // Metadata for the post
   const [postTitle, setPostTitle] = useState<string>(``);
   const [bannerLink, setBannerLink] = useState<string>(``);
   const [category, setCategory] = useState<string>(``);
+  const [blogText, setBlogText] = useState(``);
 
-  // Main content
-  const [markdown, setMarkdown] = useState(``);
+  const onSubmit = async () => {
+    try {
+      const docRef = await addDoc(collection(db, "blogs"), {
+        postTitle,
+        bannerLink,
+        category,
+        blogText,
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  };
 
   return (
     <main className="markdown-page h-full">
@@ -55,62 +53,18 @@ export default function Writer() {
             value={category}
             onChange={(value) => setCategory(value)}
           />
-          {/* <Listbox value={categoryList} onChange={setCategoryList}>
-            <div className="relative text-4xl">
-              <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white px-6 py-3 text-left text-lg shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 md:text-xl">
-                <span className="block truncate">{categoryList.name}</span>
-                <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                  👇
-                </span>
-              </Listbox.Button>
-              <Transition
-                as={Fragment}
-                leave="transition ease-in duration-100"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0"
-              >
-                <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
-                  {categories.map((category, categoryIdx) => (
-                    <Listbox.Option
-                      key={categoryIdx}
-                      className={({ active }) =>
-                        `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                          active
-                            ? "bg-amber-100 text-amber-900"
-                            : "text-gray-900"
-                        }`
-                      }
-                      value={category}
-                    >
-                      {({ selected }) => (
-                        <>
-                          <span
-                            className={`block truncate ${
-                              selected ? "font-medium" : "font-normal"
-                            }`}
-                          >
-                            {category.name}
-                          </span>
-                          {selected ? (
-                            <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
-                              ✅
-                            </span>
-                          ) : null}
-                        </>
-                      )}
-                    </Listbox.Option>
-                  ))}
-                </Listbox.Options>
-              </Transition>
-            </div>
-          </Listbox> */}
         </div>
       </div>
-      <div className="mb-4 ">
-        <div className="h-3/4 w-full"></div>
-      </div>
+      <ReactQuill
+        className="mb-4 h-full w-full"
+        theme="snow"
+        value={blogText}
+        onChange={setBlogText}
+      />
       <div className="mb-4 flex justify-end">
-        <div className="w-28 rounded-xl bg-green-300"></div>
+        <button className="focus:shadow-outline m-2 h-10 rounded-lg bg-green-700 px-5 text-green-100 transition-colors duration-150 hover:bg-green-800">
+          Success
+        </button>
       </div>
     </main>
   );
