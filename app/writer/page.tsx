d15b17cd74f50b257a82bfe02fe2a7b855532a45
Anzar Ahmad Malik <12586091+Mediocorite@users.noreply.github.com>
2024-02-02 23:03:00 +0000

    UI: Writer Listbox implementation

--- app/writer/page.tsx
+++ app/writer/page.tsx
@@ -1,11 +1,21 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import Markdown from "react-markdown";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCirclePlus } from "@fortawesome/free-solid-svg-icons";
 import Image from "next/image";
+import { Listbox, Transition } from "@headlessui/react";
+
+const categories = [
+  { id: 1, name: "Durward Reynolds", unavailable: false },
+  { id: 2, name: "Kenton Towne", unavailable: false },
+  { id: 3, name: "Therese Wunsch", unavailable: false },
+  { id: 4, name: "Benedict Kessler", unavailable: true },
+  { id: 5, name: "Katelyn Rohan", unavailable: false },
+];
+
 export default function Writer() {
   // TODO: Hookup text area to be converted into markdown.
   // const { data: session } = useSession();
@@ -22,35 +32,15 @@ export default function Writer() {
   // Metadata for the post
   const [postTitle, setPostTitle] = useState(``);
   const [bannerLink, setBannerLink] = useState(``);
-  const [categoryList, setCategoryList] = useState([]);
+  const [categoryList, setCategoryList] = useState(categories[0]);
 
   // Main content
   const [markdown, setMarkdown] = useState(``);
 
   return (
     <main className="markdown-page h-full">
-      <div className="mb-4 flex w-full flex-col text-4xl lg:flex-row">
+      <div className="mb-4 flex w-full flex-col text-2xl md:text-4xl lg:flex-row">
         <div className="relative mr-2 flex w-full lg:w-9/12">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            className="z-10 h-full w-full opacity-0 hover:opacity-100"
-          >
-            <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-5v5h-2v-5h-5v-2h5v-5h2v5h5v2z" />
-          </svg>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            className="absolute left-0 top-0 h-full w-full opacity-100 hover:opacity-100"
-          >
-            <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-5v5h-2v-5h-5v-2h5v-5h2v5h5v2z" />
-          </svg>
-
-          {/* <FontAwesomeIcon icon={faCirclePlus} /> */}
           <input
             placeholder="Title..."
             value={postTitle}
@@ -58,7 +48,57 @@ export default function Writer() {
             className="focus:outline-none "
           />
         </div>
-        <div className="w-full bg-zinc-600 lg:w-3/12"></div>
+        <div className="w-full lg:w-3/12">
+          <Listbox value={categoryList} onChange={setCategoryList}>
+            <div className="relative text-4xl">
+              <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white px-6 py-3 text-left text-lg shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 md:text-xl">
+                <span className="block truncate">{categoryList.name}</span>
+                <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                  👇
+                </span>
+              </Listbox.Button>
+              <Transition
+                as={Fragment}
+                leave="transition ease-in duration-100"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+                  {categories.map((category, categoryIdx) => (
+                    <Listbox.Option
+                      key={categoryIdx}
+                      className={({ active }) =>
+                        `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                          active
+                            ? "bg-amber-100 text-amber-900"
+                            : "text-gray-900"
+                        }`
+                      }
+                      value={category}
+                    >
+                      {({ selected }) => (
+                        <>
+                          <span
+                            className={`block truncate ${
+                              selected ? "font-medium" : "font-normal"
+                            }`}
+                          >
+                            {category.name}
+                          </span>
+                          {selected ? (
+                            <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                              ✅
+                            </span>
+                          ) : null}
+                        </>
+                      )}
+                    </Listbox.Option>
+                  ))}
+                </Listbox.Options>
+              </Transition>
+            </div>
+          </Listbox>
+        </div>
       </div>
       <div className="mb-4 flex flex-col lg:flex-row">
         <div className="h-3/4 min-h-12 w-full bg-red-600 lg:w-1/2"></div>
