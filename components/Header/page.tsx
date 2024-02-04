e70e82ea83d5048ba81f0d8b25489189f015fc99
Anzar Ahmad Malik <12586091+Mediocorite@users.noreply.github.com>
2024-02-04 20:58:38 +0000

    UIFix: Navigation fixed

--- components/Header/page.tsx
+++ components/Header/page.tsx
@@ -1,29 +1,23 @@
 import React from "react";
-import Image from "next/image";
 import Link from "next/link";
 import ThemeToggle from "./ThemeToggle";
 import AuthLinks from "./AuthLinks";
+import categories from "@/public/category.json";
 import { Drawer, DrawerContent, DrawerTrigger } from "../shadcn/ui/drawer";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin, faInstagram } from "@fortawesome/free-brands-svg-icons";
 
-const links = [
-  { id: 1, name: "About", url: "/about" },
-  { id: 2, name: "Contact", url: "/contact" },
-  { id: 3, name: "Resume", url: "/resume" },
-];
-
 export default function Header() {
   return (
     <div className="flex h-28 items-center justify-between">
-      <div className="flex flex-1 text-center text-lg font-bold md:text-2xl lg:text-left xl:text-3xl">
-        <Link href={"/"}>Jung and the Restless</Link>
+      <div className="flex text-center text-lg font-bold md:text-2xl lg:text-left xl:text-3xl">
+        <Link href={"/"}>Jung and Restless</Link>
       </div>
-      <div className="hidden flex-1 justify-end gap-5 text-lg md:flex xl:text-lg">
+      <div className="hidden flex-1 justify-end gap-5 text-lg lg:flex xl:text-lg">
         <ThemeToggle initialTheme="light" />
-        {links.map((link) => (
-          <Link key={link.id} href={link.url}>
-            {link.name}
+        {Object.values(categories).map((category) => (
+          <Link key={category.id} href={category.link}>
+            {category.name}
           </Link>
         ))}
         <AuthLinks />
@@ -39,44 +33,45 @@ export default function Header() {
             </Link>
           </div>
         </div>
-        <div className="flex md:hidden">
-          hello
-          <Drawer>
-            <DrawerTrigger>
-              hello
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="icon icon-tabler icon-tabler-menu-2"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                strokeWidth="2"
-                stroke="currentColor"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M4 6l16 0" />
-                <path d="M4 12l16 0" />
-                <path d="M4 18l16 0" />
-              </svg>
-            </DrawerTrigger>
-            <DrawerContent>
-              <div className="flex flex-col items-center justify-center">
-                {links.map((link) => (
-                  <Link
-                    key={link.id}
-                    href={link.url}
-                    className="m-4 p-4 text-center text-2xl"
-                  >
-                    {link.name}
-                  </Link>
-                ))}
+      </div>
+      <div className="block lg:hidden">
+        <Drawer>
+          <DrawerTrigger>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="icon icon-tabler icon-tabler-menu-2"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              strokeWidth="2"
+              stroke="currentColor"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M4 6l16 0" />
+              <path d="M4 12l16 0" />
+              <path d="M4 18l16 0" />
+            </svg>
+          </DrawerTrigger>
+          <DrawerContent>
+            <div className="flex flex-col items-center justify-center">
+              {Object.values(categories).map((categories) => (
+                <Link
+                  key={categories.id}
+                  href={categories.link}
+                  className="m-4 p-4 text-center text-2xl"
+                >
+                  {categories.name}
+                </Link>
+              ))}
+              <div className="m-4 p-4 text-center text-2xl">
+                Switch to dark mode
               </div>
-            </DrawerContent>
-          </Drawer>
-        </div>
+            </div>
+          </DrawerContent>
+        </Drawer>
       </div>
     </div>
   );
