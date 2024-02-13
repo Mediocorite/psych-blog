b2f7cb8405c2ddda7c7acf86cf3a9fff346e9a08
Anzar Ahmad Malik <12586091+Mediocorite@users.noreply.github.com>
2024-02-13 11:12:39 +0000

    Updating the Footer nav links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,6 @@
-import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-
+import categories from "@/public/category.json";
 function Footer() {
   return (
     <div className="mt-12 flex flex-col items-center justify-between gap-12 p-0 text-[var(--softTextColor)] lg:flex-row lg:gap-0">
@@ -18,17 +17,16 @@ function Footer() {
       </div>
       <div className="flex lg:justify-end">
         <div className="m-6 flex flex-col gap-2.5 font-light">
-          <span className="font-bold">Links</span>
-          <Link href="/">Homepage</Link>
-          <Link href="/about">About</Link>
-          <Link href="/contact">Contact</Link>
-        </div>
-        <div className="m-6 flex flex-col gap-2.5 font-light">
           <span className="font-bold">Categories</span>
-          <Link href="/">Style</Link>
-          <Link href="/">Fashion</Link>
-          <Link href="/">Coding</Link>
-          <Link href="/">Travel</Link>
+          {Object.values(categories).map((category) => (
+            <Link
+              key={category.id}
+              href={`/posts?category=${category.value}`}
+              // className="m-4 p-4 text-center text-2xl lg:m-0 lg:p-0 lg:text-xl"
+            >
+              {category.label}
+            </Link>
+          ))}
         </div>
         <div className="m-6 flex flex-col gap-2.5 font-light">
           <span className="font-bold">Social</span>
